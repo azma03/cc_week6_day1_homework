42734c5382d5837a76d7f00c53fd220cf2325aa5
Asma <asma.virgo85@gmail.com>
2018-10-09 00:22:14 +0100

    JS scope exercise

---
+++ homework_scope.js
@@ -0,0 +1,233 @@
+// https://gist.github.com/johnmcc/7130f2046f27f6659b2020110ecf6ce5
+
+// Scope Homework: Who Dunnit
+//
+// Learning Objectives
+//
+// Understand function scope
+// Know the difference in between the let and const keywords
+// Brief
+//
+// Using your knowledge about scope and variable declarations in JavaScript, look at the following code snippets and predict what the output or error will be and why. Copy the following episodes into a JavaScript file and add comments under each one detailing the reason for your predicted output.
+//
+// MVP
+
+// Episode 1
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// Output:  The murderer is Miss Scarlet.
+//Reason: Lexixal scope - scneario is visible in the nested block
+
+
+// Episode 2
+//
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// // changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+//Output: changeMurderer() would give error as it is trying to change the const variable
+//Output: The murderer is Professor Plum
+
+// Episode 3
+
+// let murderer = 'Professor Plum';
+//
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+//
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+
+// Output: First Verdict:  The murderer is Mrs. Peacock.  //as the function is assigning value to a locally scoped 'let' varibale.
+// Output: Second Verdict:  The murderer is Professor Plum. //still prints the inital value as the other function only updated the local variable.
+
+
+// Episode 4
+
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+//
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+//
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+
+// Output: The suspects are Miss Scarlet, Professor Plum, Colonel Mustard. // as suspectThree is updated locally in the function
+// Output: Suspect three is Mrs. Peacock.  //suspectThree still holds the initial value
+
+// Episode 5
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Kitchen',
+//   weapon: 'Candle Stick'
+// };
+//
+// const changeWeapon = function(newWeapon) {
+//   scenario.weapon = newWeapon;
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is the ${scenario.weapon}.`;
+// }
+//
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+// Output: The weapon is the Revolver.
+//  Reason: scenario is const but its properties can be changed.
+
+
+// Episode 6
+
+// let murderer = 'Colonel Mustard';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+//
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
+//
+//   plotTwist();
+// }
+//
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// Output: The murderer is Mrs. White.
+// Reason: plotTwist is a function within changeMurderer and it will set the value of murderer to "Mrs. White" each time the changeMurderer function is called
+
+
+// Episode 7
+
+// let murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+//
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+//
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+//
+//     unexpectedOutcome();
+//   }
+//
+//   plotTwist();
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// Output: The murderer is Mr. Green.
+// Reason: plotTwist is declaring its own temprary variable so this function and the one inside (unexpectedOutcome) are only updating the local variable instead of the outer variable.
+
+
+// Episode 8
+
+// const scenario = {
+//   murderer: 'Mrs. Peacock',
+//   room: 'Conservatory',
+//   weapon: 'Lead Pipe'
+// };
+//
+// const changeScenario = function() {
+//   scenario.murderer = 'Mrs. Peacock';
+//   scenario.room = 'Dining Room';
+//
+//   const plotTwist = function(room) {
+//     if (scenario.room === room) {
+//       scenario.murderer = 'Colonel Mustard';
+//     }
+//
+//     const unexpectedOutcome = function(murderer) {
+//       if (scenario.murderer === murderer) {
+//         scenario.weapon = 'Candle Stick';
+//       }
+//     }
+//
+//     unexpectedOutcome('Colonel Mustard');
+//   }
+//
+//   plotTwist('Dining Room');
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is ${scenario.weapon}.`
+// }
+//
+// changeScenario();
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+// Output: The weapon is Candle Stick.
+// the if condition holds true for the function unexpectedOutcome as the properties of the const (one level above) were updated.
+
+
+// Episode 9
+// 
+// let murderer = 'Professor Plum';
+//
+// if (murderer === 'Professor Plum') {
+//   let murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+//Output: The murderer is Professor Plum.
+//Reason: the if statement is only updating a local temporary variable and therfore the initial variable remains unchanged.
+
+// Extensions
+//
+// Make up your own episode!
